315e2874b402b549ea9f1c1e88dad5a28ed14df8
CedricLeon <cedric.leonard@insa-rennes.fr>
2021-01-24 19:11:16 +0100

    Adding Training Instructions in main + params.json

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,22 @@
 #include <cstdlib>
 #include <ctime>
 
-/*#include <numeric>
+#define _USE_MATH_DEFINES // To get M_PI
+
+#include <math.h>
+#include <numeric>
 #include <thread>
 #include <atomic>
 #include <chrono>
-#include <inttypes.h>*/
+#include <inttypes.h>
 
 #include <gegelati.h>
-#define _USE_MATH_DEFINES // To get M_PI
-
-class MultByConstParam;
-
-#include <cmath>
 
 #include "../include/game2048.h"
+
+#ifndef NB_GENERATIONS
+#define NB_GENERATIONS 1200
+#endif
 
 // TPG training
 int main() {
@@ -23,7 +25,7 @@ int main() {
     std::cout << "Start 2048 application." << std::endl;
 
     // Create the instruction set for programs
-    /*Instructions::Set set;
+    Instructions::Set set;
     auto minus = [](double a, double b)->double {return a - b; };
     auto add = [](double a, double b)->double {return a + b; };
     auto mult = [](double a, double b)->double {return a * b; };
@@ -31,10 +33,10 @@ int main() {
     auto max = [](double a, double b)->double {return std::max(a, b); };
     auto ln = [](double a)->double {return std::log(a); };
     auto exp = [](double a)->double {return std::exp(a); };
-    auto cos = [](double a)->double {return std::cos(a); };
-    auto sin = [](double a)->double {return std::sin(a); };
-    auto tan = [](double a)->double {return std::tan(a); };
-    auto pi = [](double a)->double {return M_PI; };
+    //auto cos = [](double a)->double {return std::cos(a); };
+    //auto sin = [](double a)->double {return std::sin(a); };
+    //auto tan = [](double a)->double {return std::tan(a); };
+    //auto pi = [](double a)->double {return M_PI; };
 
     set.add(*(new Instructions::LambdaInstruction<double, double>(minus)));
     set.add(*(new Instructions::LambdaInstruction<double, double>(add)));
@@ -43,32 +45,98 @@ int main() {
     set.add(*(new Instructions::LambdaInstruction<double, double>(max)));
     set.add(*(new Instructions::LambdaInstruction<double>(exp)));
     set.add(*(new Instructions::LambdaInstruction<double>(ln)));
-    set.add(*(new Instructions::LambdaInstruction<double>(cos)));
-    set.add(*(new Instructions::LambdaInstruction<double>(sin)));
-    set.add(*(new Instructions::LambdaInstruction<double>(tan)));
-    set.add(*(new Instructions::MultByConstant<double>()));     // float ?
-    set.add(*(new Instructions::LambdaInstruction<double>(pi)));*/
+    //set.add(*(new Instructions::LambdaInstruction<double>(cos)));
+    //set.add(*(new Instructions::LambdaInstruction<double>(sin)));
+    //set.add(*(new Instructions::LambdaInstruction<double>(tan)));
+    set.add(*(new Instructions::MultByConstant<double>()));     // <double, float>
+    //set.add(*(new Instructions::LambdaInstruction<double>(pi)));
 
     // Initialisation
+    Learn::LearningParameters params;
+    File::ParametersParser::loadParametersFromJson(ROOT_DIR "/params.json", params);
 
-    auto game = new game2048();
-    //initGame(); // Done in default contructor
-    game->printUI();
-    char command;
+    // Instantiate the LearningEnvironment
+    game2048 gameLE;
 
-    // Main loop which break if user lost, won or enter the leave char (L or l)
-    while (true)
+    std::cout << "Number of threads: " << std::thread::hardware_concurrency() << std::endl;
+
+    // Instantiate and init the learning agent
+    Learn::ParallelLearningAgent la(gameLE, set, params);
+    la.init();
+
+    const TPG::TPGVertex* bestRoot = NULL;
+
+    // Start a thread for controlling the loop
+#ifndef NO_CONSOLE_CONTROL
+    // Console
+    std::atomic<bool> exitProgram = true; // (set to false by other thread)
+    std::atomic<bool> toggleDisplay = true;
+    std::atomic<bool> doDisplay = false;
+    std::atomic<uint64_t> generation = 0;
+
+    //std::thread threadDisplay(std::ref(exitProgram), std::ref(toggleDisplay), std::ref(doDisplay), &bestRoot, std::ref(set), std::ref(gameLE), std::ref(params), std::ref(generation));
+
+    while (exitProgram); // Wait for other thread to print key info.
+#else
+    std::atomic<bool> exitProgram = false; // (set to false by other thread)
+	std::atomic<bool> toggleDisplay = false;
+#endif
+
+    // Basic logger
+    Log::LABasicLogger basicLogger(la);
+
+    // Create an exporter for all graphs
+    File::TPGGraphDotExporter dotExporter("out_0000.dot", la.getTPGGraph());
+
+    // Logging best policy stat.
+    std::ofstream stats;
+    stats.open("bestPolicyStats.md");
+    Log::LAPolicyStatsLogger policyStatsLogger(la, stats);
+
+    for (int i = 0; i < NB_GENERATIONS && !exitProgram; i++)
     {
-        std::cout << "Choose a direction : ";
-        std::cin >> command;
-        if((command == 76 || command == 108) || !game->move(command)) // ('l' or 'L' to leave) or (lost or win)
+        char buff[13];
+        sprintf(buff, "out_%04d.dot", i);
+        dotExporter.setNewFilePath(buff);
+        dotExporter.print();
+
+        la.trainOneGeneration(i);
+
+#ifndef NO_CONSOLE_CONTROL
+        generation = i;
+        if (toggleDisplay && !exitProgram)
         {
-            std::cout << "Leaving the Game" << std::endl;
-            break;
+            bestRoot = la.getBestRoot().first;
+            doDisplay = true;
+            while (doDisplay && !exitProgram);
         }
-        game->printUI();
+#endif
     }
-    delete game;
+
+    // Keep best policy
+    la.keepBestPolicy();
+    dotExporter.setNewFilePath("out_best.dot");
+    dotExporter.print();
+
+    TPG::PolicyStats ps;
+    ps.setEnvironment(la.getTPGGraph().getEnvironment());
+    ps.analyzePolicy(la.getBestRoot().first);
+    std::ofstream bestStats;
+    bestStats.open("out_best_stats.md");
+    bestStats << ps;
+    bestStats.close();
+    stats.close();
+
+    // cleanup
+    for (unsigned int i = 0; i < set.getNbInstructions(); i++)
+        delete (&set.getInstruction(i));
+
+#ifndef NO_CONSOLE_CONTROL
+    // Exit the thread
+    std::cout << "Exiting program, press a key then [enter] to exit if nothing happens.";
+    //threadDisplay.join();
+#endif
+
     return 0;
 }
 
